df39efd902597e7fe684faac20aee8452d5f5718
Kelvin Maure <kmaure16@gmail.com>
2022-06-23 20:43:28 -0400

    task 4

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const sqlite3=require('sqlite3').verbose();
 const path = require('path');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
+const { count, info } = require('console');
+const nodemailer = require ('nodemailer');
+const passport = require ('passport');
+const { appendFile } = require('fs');
+const session = require ('express-session');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+require('dotenv').config();
+
 
 const basededatos=path.join(__dirname,"BD","BD.db");
 const bd=new sqlite3.Database(basededatos, err =>{ 
@@ -13,7 +21,7 @@ if (err){
 }
 })
 
-const create="CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip TEXT,ubi VARCHAR(20));";
+const create="CREATE TABLE IF NOT EXISTS contactos(email VARCHAR(20),nombre VARCHAR(20), comentario TEXT,fecha DATATIME,ip VARCHAR(30),ubi VARCHAR(20));";
 
 bd.run(create,err=>{
 	if (err){
@@ -35,6 +43,77 @@ router.get('/contactos',(req,res)=>{
 })
 
 
+router.use(session({
+	secret:'Super secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+router.use(passport.authenticate('session'));
+
+passport.serializeUser(function(user, cb) {
+	process.nextTick(function() {
+	  cb(null, { id: user.id, username: user.username, name: user.name });
+	});
+  });
+
+  passport.deserializeUser(function(user, cb) {
+	process.nextTick(function() {
+	  return cb(null, user);
+	});
+  });
+
+
+
+router.get('/login', (req,res) => {
+	res.render("login")
+});
+
+
+const usser = process.env.USSER;
+const pass = process.env.PASSW;
+router.post('/login', (req,res) => {
+	 var usuario=req.body.usuario;
+	  var contra=req.body.contra;
+		console.log(usuario,contra)
+	if (usuario === usser && contra === pass) {
+	
+	res.redirect('/contactos')
+	}else{
+		res.send("Usuario invalido")
+	}
+	
+});
+
+router.get('/login/federated/google', passport.authenticate('google'));
+
+
+passport.use(new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SC,
+      callbackURL: "https://nemesis2022.herokuapp.com/redirect/google",
+	  scope: [
+		"https://www.googleapis.com/auth/userinfo.profile",
+		"https://www.googleapis.com/auth/userinfo.email",
+	  ],
+	  session: false,
+    },
+    function (accessToken, refreshToken, profile, done) {
+		console.log(profile); 
+		done(null, profile)
+      }));
+
+
+
+	  router.get('/redirect/google', passport.authenticate('google', {
+		successRedirect: '/contactos',
+		failureRedirect: '/login'
+	  }));
+
+
+
+
 router.post('/',(req,res)=>{
   	var hoy = new Date();
   	var horas = hoy.getHours();
@@ -42,17 +121,6 @@ router.post('/',(req,res)=>{
   	var segundos = hoy.getSeconds()
   	var hora = horas + ':' + minutos + ':' + segundos + ' '
   	var fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear() + '//' + hora;
-	const url = 'http://www.geoplugin.net/json.gp?ip=' + ip;
-	//fetch('http://www.geoplugin.net/json.gp?ip=' + ip_client)
-    // Exito
-	ip_rick = '186.92.150.56'
-	let country_ip;
-    fetch('http://www.geoplugin.net/json.gp?ip=' + ip_rick)
-    .then(response => response.json())
-    .then(json => {country_ip = json.geoplugin_countryName
-
-    }
-);
 
 	var ip = req.headers["x-forwarded-for"];
   	if (ip){
@@ -61,17 +129,80 @@ router.post('/',(req,res)=>{
  	 } else {
 	  ip = req.connection.remoteAddress;
   	}
+	
+	let country_ip;
+    fetch('http://www.geoplugin.net/json.gp?ip=' + country_ip)
+    .then(response => response.json())
+    .then(json => {country_ip = json.geoplugin_countryName
+		
+
+   let cacha;
+  	const requi = req.body['g-recaptcha-response']
+	const priv = process.env.API_PRIVATE;
+	const url = `https://www.google.com/recaptcha/api/siteverify?secret=${priv}&response=${requi}`;
+	fetch(url, {
+    method: 'post',
+ 	})
+    .then((response) => response.json())
+    .then((google_response) => {
+      if (google_response.success == true) {		
+		console.log('Captcha correcto'); 
+	
 	const sql="INSERT INTO contactos(nombre, email, comentario, fecha ,ip,ubi) VALUES (?,?,?,?,?,?)";
 	const nuevos_mensajes=[req.body.nombre, req.body.email, req.body.comentario,fecha,ip, country_ip];
 	bd.run(sql, nuevos_mensajes, err =>{
-	if (err){
-		return console.error(err.message);
+	if (err){return console.error(err.message);}
+	else{	res.redirect("/");}	})
+
+	var transporter = nodemailer.createTransport({
+		host: "smtp.ethereal.email",
+		post: 587,
+		secure:false,
+		auth: {
+			user: procces.env.USSER,
+			pass: process.env.PASSW,
+		},
+	});
+	
+	var mailOptions = {
+		from: procces.env.REMITENTE,
+		to: 'example@gmail.com',
+		subject: "Programacion II",
+		text: 'Task 3',
+		html:  `<p>Nombre: ${req.body.nombre}<p>
+						<p>Email: ${req.body.email}<p>
+						<p>Mensaje: ${req.body.comentario}
+						<p>Fecha: ${fecha}
+						<p>Ip: ${ip}
+						<P>Pais: ${country_ip}` 
 	}
-	else{
-		res.redirect("/");
-		}
-	})
+
+	transporter.sendMail(mailOptions, (error,info) => {
+			if(error){
+				console.log(error.message);
+			}
+			else{
+				console.log("Email Enviado")
+			}
+	});
+	
+} 
+	
+	
+	else{	
+		res.redirect('/')
+		console.log('Resuelve el captcha')
+			}	
+	
+
+	    }
+	) }
+);
 });
+
+
+
+
 
 
 router.get('/',(req,res)=>{
